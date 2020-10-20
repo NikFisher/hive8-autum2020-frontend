5f3ac205e4ab2b2e6261f460a7a83e59fbb9e61f
John <deus-ex-machina@Johns-MacBook-Pro.local>
2020-10-20 13:47:50 +0200

    fixed faulty react-router and react-router-dom version descripancy

--- src/components/ToDoListItem/index.jsx
+++ src/components/ToDoListItem/index.jsx
@@ -1,22 +1,14 @@
 /* eslint-disable no-console */
 import React from 'react';
-import PropTypes, { bool } from 'prop-types';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import ToDoListItemStyled from './ToDoListItemStyled';
-import Todo from '../../compositions/Todo/index';
-import H3 from '../H3/index';
-import H2 from '../H2/index';
-import H1 from '../H1/index';
 import Button from '../Button/index';
 import Grid from '../Grid/index';
-import Link from '../Link/index';
 import GridChild from '../GridChild/index';
 import Paragraph from '../Paragraph/index';
 import breakpoints from 'helpers/constants/breakpoints.mjs';
-import { useState } from 'react';
-import EditTaskView from '../../containers/EditTaskView/index';
-
-var isChecked = false;
 
 /*function setCheck(newCheck){
 	isChecked = newCheck;
@@ -26,38 +18,40 @@ var isChecked = false;
 }*/
 //const [check, setCheck2] = useState(false);
 
-const ToDoListItem = props => (
-	<ToDoListItemStyled {...props} disabled={props.disabled}>
-		<Grid columns={4}>
-			<GridChild columnSpan={[{ columns: 4 }, { break: breakpoints.mobile, columns: 1 }]}>
-				<Paragraph>Completed</Paragraph>
-				<input type="checkbox" onChange={props.onChange} checked={props.checked} />
-			</GridChild>
-			<GridChild columnSpan={[{ columns: 4 }, { break: breakpoints.mobile, columns: 1 }]}>
-				<h2> {props.children}</h2>
-			</GridChild>
-			<GridChild columnSpan={[{ columns: 4 }, { break: breakpoints.mobile, columns: 1 }]}>
-				<Link
-					to={{
-						pathname: `EditTaskView/${props.task.taskID}`,
-						state: { taskObject: props.task },
-						exact: true
-					}}
-				>
-					<Button onClick={props.edit}>Edit</Button>
-				</Link>
-				<Button onClick={props.delete}>Delete</Button>
-			</GridChild>
-		</Grid>
-	</ToDoListItemStyled>
-);
+const ToDoListItem = props => {
+	console.log('PROPS EDIT', props.edit);
+	return (
+		<ToDoListItemStyled {...props} disabled={props.disabled}>
+			<Grid columns={4}>
+				<GridChild columnSpan={[{ columns: 4 }, { break: breakpoints.mobile, columns: 1 }]}>
+					<Paragraph>Completed</Paragraph>
+					<input type="checkbox" onChange={props.onChange} checked={props.checked} />
+				</GridChild>
+				<GridChild columnSpan={[{ columns: 4 }, { break: breakpoints.mobile, columns: 1 }]}>
+					<h2> {props.children}</h2>
+				</GridChild>
+				<GridChild columnSpan={[{ columns: 4 }, { break: breakpoints.mobile, columns: 1 }]}>
+					<Link
+						to={{
+							pathname: `/EditTaskView/${props.task.taskID}`,
+							state: { taskObject: props.task }
+						}}
+					>
+						<Button>Edit</Button>
+					</Link>
+					<Button onClick={props.delete}>Delete</Button>
+				</GridChild>
+			</Grid>
+		</ToDoListItemStyled>
+	);
+};
 
 ToDoListItem.propTypes = {
 	children: PropTypes.string.isRequired,
 	disabled: PropTypes.bool,
-	delete: PropTypes.function,
-	edit: PropTypes.function,
-	onChange: PropTypes.function,
+	delete: PropTypes.func,
+	edit: PropTypes.func,
+	onChange: PropTypes.func,
 	checked: PropTypes.bool,
 	nameOfTask: PropTypes.string,
 	task: PropTypes.object
